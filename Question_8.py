154d666ce2d23dc824a493140c51e6681527c750
hassankhann18 <hassanaddy11@gmail.com>
2026-01-30 19:36:28 -0700

    Adding Q8

---
+++ Question_8.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "A": [1, 2, 2, 1],
+    "B": [3.1, 4.2, 1.5, 6.3],
+    "C": [800, 150, 400, 210]
+}
+df = pd.DataFrame(data)
+
+# add a computed column
+df["D"] = df["A"] * df["B"]
+
+print(df)
