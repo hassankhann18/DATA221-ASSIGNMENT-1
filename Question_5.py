005887e7317416a5b6e9535eff30cb1d9d7089b8
hassankhann18 <hassanaddy11@gmail.com>
2026-01-30 17:29:45 -0700

    Adding Q5 - using basic comments in the code

---
+++ Question_5.py
@@ -0,0 +1,26 @@
+#NAME : HASSAN ADNAN
+#UCID: 30217418
+
+def circleAreaCoverage(radiusOfCircle1, radiusOfCircle2):
+
+
+    if radiusOfCircle1 <= 0 or radiusOfCircle2 <= 0:  # Validation for input
+        return "Radii should be a positive integers"
+
+   # Calculating areas
+    pi = 3.14159
+    areaOfCircle1 = pi * radiusOfCircle1 * radiusOfCircle1
+    areaOfCircle2 = pi * radiusOfCircle2 * radiusOfCircle2
+
+    # determine smaller and larger circle
+    if areaOfCircle1 < areaOfCircle2:
+        smaller = areaOfCircle1
+        larger = areaOfCircle2
+    else:
+        smaller = areaOfCircle2
+        larger = areaOfCircle1
+
+    # calculate percentage coverage
+    percentage = (smaller / larger) * 100
+
+    return percentage
