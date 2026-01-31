663c20675b6423199c60e4c9734c8e6c1dca2c57
hassankhann18 <hassanaddy11@gmail.com>
2026-01-30 23:25:51 -0700

    Adding Answers

--- Question_4.py
+++ Question_4.py
@@ -12,11 +12,9 @@ for i in range(len(values)):
     if values[i] >= x:
         indices.append(i)
 
-# print the sorted list and x
 print("Sorted values:", values)
 print("x:", x)
 
-#  print the first matching index (if it exists)
 if len(indices) > 0:
     print("First matching index:", indices[0])
 else:
--- Question_5.py
+++ Question_5.py
@@ -7,7 +7,6 @@ def circleAreaCoverage(radiusOfCircle1, radiusOfCircle2):
     if radiusOfCircle1 <= 0 or radiusOfCircle2 <= 0:  # Validation for input
         return "Radii should be a positive integers"
 
-   # Calculating areas
     pi = 3.14159
     areaOfCircle1 = pi * radiusOfCircle1 * radiusOfCircle1
     areaOfCircle2 = pi * radiusOfCircle2 * radiusOfCircle2
